237680d073f747c2a56692e4d18496e25eed2c18
AlexLevanchenko <99507725+AlexLevanchenko@users.noreply.github.com>
2022-02-18 10:19:17 +0200

    Update InchConvert.java

--- src/InchConvert.java
+++ src/InchConvert.java
@@ -4,9 +4,10 @@ public class InchConvert {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите значение для дюйма: ");
+        final double meterInInch = 0.0254;
         double inch, meter;
         inch = input.nextInt();
-        meter = inch*0.0254;
+        meter = inch * meterInInch;
         System.out.println(inch + " дюйма - это "+ meter + " метра");
     }
 }
